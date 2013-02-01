789ffa7d60f8f2f5e058295b3155326fd9502a36
Tycho Andersen <tycho@tycho.ws>
2013-02-01 06:54:01 -0600

    Some semblance of static site generation

--- src/Text/Haggis.hs
+++ src/Text/Haggis.hs
@@ -9,6 +9,8 @@ import Control.Applicative
 
 import qualified Data.ByteString.Lazy as BS
 import Data.Either
+import qualified Data.Map as M
+import Data.Maybe
 import Data.Time.Calendar
 
 import System.Posix.Files.ByteString
@@ -35,6 +37,40 @@ data Page = Page {
   pageContent :: [Node]
 }
 
+data MultiPageType =
+  Tag String |
+  DirIndex FilePath |
+  Archive Integer (Maybe Int)
+
+mpTypeToPath :: MultiPageType -> FilePath
+mpTypeToPath (Tag t) = "tags" </> t <.> "html"
+mpTypeToPath (DirIndex d) = d </> "index.html"
+mpTypeToPath (Archive y m) =
+  let month = fromMaybe "" $ fmap show m
+  in "archives" </> show y </> month <.> "html"
+
+mpTypeToTitle :: MultiPageType -> String
+mpTypeToTitle (Tag t) = "Tagged: " ++ t
+mpTypeToTitle (DirIndex d) = "Filed under: " ++ d
+mpTypeToTitle (Archive y m) =
+  let month = fromMaybe "" $ fmap ((" - " ++) . show) m
+  in "Posts from: " ++ (show y) ++ month
+
+data MultiPage = MultiPage {
+  pages :: [Page],
+  multiPageType :: MultiPageType
+}
+
+bindPage :: Page -> [Node] -> [Node]
+bindPage Page { pageTitle = title
+              , pageTags = tags
+              , pageDate = date
+              , pageContent = content
+              } = hq ".title" title .
+                  hq ".tags" tags .
+                  hq ".date" (fmap show date) .
+                  hq ".content" content
+
 buildPage :: FilePath -> [Node] -> Page
 buildPage fp ns = Page { pageTitle = "foo"
                        , pageTags = []
@@ -54,16 +90,51 @@ buildSite src tgt = do
   actions <- collectSiteElements src tgt templates
   let (raws, pages) = partitionEithers actions
   readPages <- sequence pages
-  sequence_ $ raws ++ map writePage readPages
+  let (multiPages, tags) = generateIndexesAndTags readPages
+  ps <- sequence pages
+  writeSite ps multiPages templates tags
+  sequence_ raws
+
+writeSite :: [Page] -> [MultiPage] -> SiteTemplates -> ([Node] -> [Node]) -> IO ()
+writeSite ps mps templates tags = do
+  sequence_ $ map writePage ps
+  sequence_ $ map writeMultiPage mps
   where
+    wrapper = tags $ root templates
+    writeThing fp title ns = do
+      let xform = hq "#content" ns . hq "#title" title
+          html = xform $ wrapper
+      ensureDirExists fp
+      BS.writeFile fp $ toLazyByteString $ renderHtmlFragment UTF8 html
     writePage :: Page -> IO ()
-    writePage p = let path = pagePath p
-                      content = pageContent p
-                      builder = renderHtmlFragment UTF8 content
-                  in BS.writeFile path $ toLazyByteString builder
+    writePage p =
+      let content = bindPage p $ single templates
+      in writeThing (pagePath p) (pageTitle p) content
+    writeMultiPage :: MultiPage -> IO ()
+    writeMultiPage mp =
+      let xform = hq ".page" $ map bindPage $ pages mp
+          content = xform $ multiple templates
+          path = mpTypeToPath $ multiPageType mp
+      in writeThing path (mpTypeToTitle $ multiPageType mp) content
 
 ensureDirExists :: FilePath -> IO ()
 ensureDirExists = createDirectoryIfMissing True . dropFileName
+
+generateIndexesAndTags :: [Page] -> ([MultiPage], [Node] -> [Node])
+generateIndexesAndTags ps =
+  let tagPages = buildMultiPage Tag tags
+      indexPages = buildMultiPage DirIndex indexes
+      tagCounts = M.toList $ M.map length tags
+      tagBind = hq ".tag" (map (\(t, c) -> t ++ " (" ++ (show c) ++ ")") tagCounts)
+  in (tagPages ++ indexPages, tagBind)
+  where
+    mapAccum :: Ord a => [(a, b)] -> M.Map a [b]
+    mapAccum = foldr (\(k,v) -> M.insertWith (++) k [v]) M.empty
+    tags = mapAccum $ concatMap (\p -> zip (pageTags p) (repeat p)) ps
+    indexes = mapAccum $ map (\p -> (dropFileName $ pagePath p, p)) ps
+    buildMultiPage :: (String -> MultiPageType) -> M.Map String [Page] -> [MultiPage]
+    buildMultiPage typeBuilder ps =
+      map (\(name, pages) -> MultiPage pages $ typeBuilder name) $ M.toList ps
 
 type Accum = [Either (IO ()) (IO Page)]
 collectSiteElements ::
@@ -88,8 +159,7 @@ collectSiteElements src tgt templates = foldWithHandler
       let path = infoPath info
       html <- renderContent path
       let target = replaceExtension (mkTgtName path) ".html"
-          page = hq "#content" html $ root templates
-      return $ buildPage path page
+      return $ buildPage target html
     makeAction info _ | isRegularFile $ infoStatus info = Left $ do
       let path = infoPath info
       ensureDirExists path

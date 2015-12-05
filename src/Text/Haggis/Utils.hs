d0d3919e6787adcbfdccc8e3f635de3ac2172fbb
Tycho Andersen <tycho@tycho.ws>
2015-12-05 16:49:40 -0700

    work around renderHtmlNodes bug
    As the comment says, renderHtmlNodes seems to butcher the rendering of
    pandoc's Html RawBlock output. As a workaround, let's render this to a
    string, then parse it again using XmlHtml's native parser, which then gets
    things right.

--- src/Text/Haggis/Utils.hs
+++ src/Text/Haggis/Utils.hs
@@ -2,19 +2,23 @@ module Text.Haggis.Utils where
 
 import Blaze.ByteString.Builder
 
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Map.Lazy as M
 import qualified Data.Set as S
 import qualified Data.Text as T
 
 import Text.Blaze.Renderer.XmlHtml
+import qualified Text.Blaze.Html.Renderer.Utf8 as PHTML
 import Text.Pandoc.Definition
 import Text.Pandoc.Error
 import Text.Pandoc.Options
 import Text.Pandoc.Writers.HTML
 import Text.XmlHtml
 
-renderHtml :: [Node] -> BS.ByteString
+import Debug.Trace
+
+renderHtml :: [Node] -> BSL.ByteString
 renderHtml = toLazyByteString . renderHtmlFragment UTF8
 
 mapAccum :: Ord a => [(a, b)] -> M.Map a [b]
@@ -22,17 +26,19 @@ mapAccum = foldr (\(k,v) -> M.insertWith (++) k [v]) M.empty
 
 pandocToHtml :: Either PandocError Pandoc -> [Node]
 pandocToHtml (Left err) = [TextNode (T.pack (show err))]
-pandocToHtml (Right doc) = renderHtmlNodes $ writeHtml writeOpts doc
+-- Text.Blaze.Renderer.XmlHtml's renderHtmlNodes doesn't render RawNodes in
+-- pandoc correctly. Instead, we render the document to a string, and re-parse
+-- it with XmlHtml's parseHTML, which parses them correctly into Nodes.
+pandocToHtml (Right doc) =
+  let rendered = PHTML.renderHtml $ writeHtml def doc
+      result = parseHTML "foo" $ (trace (show rendered) (BSL.toStrict rendered))
+  in case result of
+       Left err -> [TextNode (T.pack (show err))]
+       Right doc2 -> docContent doc2
 
 readOpts :: ReaderOptions
 readOpts = def {
   readerExtensions = S.fromList [ Ext_raw_html ],
   readerParseRaw = True,
-  readerSmart = True,
-  readerTrace = True
-}
-
-writeOpts :: WriterOptions
-writeOpts = def {
-  writerExtensions = S.fromList [ Ext_raw_html ]
+  readerSmart = True
 }
